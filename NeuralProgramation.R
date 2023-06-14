5c68063ed214c466a3d979e6db03d9763195afca
jo10ao <65049560+jo10ao@users.noreply.github.com>
2023-06-14 05:59:24 -0700

    My first code for the neural

---
+++ NeuralProgramation.R
@@ -0,0 +1,110 @@
+##### Initialisation des valeurs des parametres
+initialisation<-function(x){
+  W= matrix( runif(dim(x)),nrow =dim(x)[2],ncol = 1)
+  b= vector("numeric",  dim(x)[2]  )
+  list(W,b)
+}
+initialisation<-cmpfun(initialisation)
+##### definition de la descente de gradient
+gradient<-function(A,X,y){
+  dW=(1/dim(y)[1])*(t( X)  %*%(A-y))
+  db=(1/dim(y)[1])* sum(A-y)
+  list(dW,db)
+}
+gradient<-cmpfun(gradient)
+###### Definition du modele
+modelss<-function(X,W,b){
+  Z=X  %*%W + b
+  A= 1/(1+exp(-Z))
+  return(A)
+}
+modelss<-cmpfun(modelss)
+##
+
+
+######### Fonction de log-vraisemblance
+
+log_loss<-function(A,y){
+  o<-(1 / dim(y)[1]) * sum(-y * log(A) - (1 - y) * log(1 - A ))
+  o
+}
+log_loss<-cmpfun(log_loss)
+### Fonction de mise a jour des parametres 
+updatess<-function(dW,db,W,b,learning_rate){
+  W=W-learning_rate*dW
+  b=b-learning_rate*db
+  list(W,b)
+}
+updatess<-cmpfun(updatess)
+
+#####Fonction de prediction
+predicts<- function(X,W,b){
+  P=modelss(X,W,b)
+  P=as.data.frame (P)
+  for (i in 1:nrow(P)){
+    if (P[i,]>=0.5) (P[i,]<-1)
+    else P[i,]<-0
+  }
+  P<-as.matrix(P[,1])
+  
+  return(P)
+}
+library(compiler)
+predicts<- cmpfun(predicts)
+######### La fonction de la matrice de confusion
+matConf<- function(data=NULL,obs,pred,plot=TRUE,
+                   unit = "count",
+                   colors = c(low = NULL, high = NULL),
+                   print_metrics = TRUE,
+                   metrics_list = c("accuracy", "precision", "recall"),
+                   position_metrics = "top",
+                   na.rm = TRUE){
+  library(metrica)
+  c<-metrica::confusion_matrix(data,obs,pred,plot,
+                               unit = "count",
+                               colors = c(low = NULL, high = NULL),
+                               print_metrics = TRUE,
+                               metrics_list = c("accuracy", "precision", "recall"),
+                               position_metrics = "top",
+                               na.rm = TRUE )
+  return(c)
+}
+
+
+########## Le reseau de neurone artificiel
+
+artificial_neurone<-function(X,y,learning_rate,n_iter=1000){
+  
+  #initialisation
+  s=initialisation(X)
+  W=s[[1]]
+  b=s[[2]]
+  Loss=c()
+  p_b<-txtProgressBar(min=1,max=n_iter , style=3)
+  for (i in 1:n_iter) {
+    A=modelss(X,W,b) 
+    Loss[i]=log_loss(A,y)
+    dW=gradient(A,X,y)[[1]]
+    db=gradient(A,X,y)[[2]]
+    W=updatess(dW,db,W,b,learning_rate)[[1]]
+    b=updatess(dW,db,W,b,learning_rate)[[2]]
+    Sys.sleep(0.01) 
+    setTxtProgressBar(p_b,i)
+  }
+  
+  y_pred=predicts(X,W,b)
+  print(W)
+  print(b)
+  
+  #da=c(Loss,1:n_iter)
+  #p<-ggplot(da)%>% aes(x=1:n_iter,y=Loss)%>%geom_line()
+  #p
+  #figure=ploot(da,x=1:n_iter,y=Loss)
+  plot(x=1:n_iter,y=Loss,type="l",col="red", lwd = 1,xlab="Nombre d'iteration", 
+       ylab="Cout")
+  return(list(W=W,b=b,Loss=Loss,A=A))
+}
+
+artificial_neurone(X,y,learning_rate = 0.5  ,n_iter=150)
+library(microbenchmark)
+system.time (artificial_neurone(X,y,learning_rate = 0.1  ,n_iter=1500))
